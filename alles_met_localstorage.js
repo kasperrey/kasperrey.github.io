f17d14f51a00f464198d328345d7e74932f2b4b4
kasperrey <kasper.reynders@gmail.com>
2023-07-30 09:14:15 +0200

    aders opgeslagen

--- alles_met_localstorage.js
+++ alles_met_localstorage.js
@@ -1,41 +1,34 @@
-export function getLijst_met_personen() {
-    return JSON.parse(localStorage.getItem("letterkuil-lijst_personen"))
-}
-
-export function getPersoon(persoon) {
-    return JSON.parse(localStorage.getItem(persoon))
-}
-
-export function getGeselecteerd_persoon() {
-    return JSON.parse(localStorage.getItem("letterkuil-geselekteerd_persoon"))
-}
-
-export function getGeselecteerde_letters() {
-    return JSON.parse(localStorage.getItem("letterkuil-geselekteerde_letters"))
-}
-
-export function setLijst_met_personen(data) {
-    localStorage.setItem("letterkuil-lijst_personen", JSON.stringify(data))
-}
-
-export function setPersoon(persoon, data) {
-    return localStorage.setItem(persoon, JSON.stringify(data))
-}
-
-export function setGeselecteerd_persoon(data) {
-    return localStorage.setItem("letterkuil-geselekteerd_persoon", JSON.stringify(data))
-}
-
-export function setGeselecteerde_letters(data) {
-    return localStorage.setItem("letterkuil-geselekteerde_letters", JSON.stringify(data))
-}
-
-export function check() {
-    if (!localStorage.getItem("letterkuil-lijst_personen")) {
-        localStorage.setItem("letterkuil-lijst_personen", JSON.stringify([]))
+window.alles_met_localstorage = {
+    "getLijst_met_personen": function () {
+        return JSON.parse(localStorage.getItem("letterkuil-lijst_personen"))
+    },
+    "getPersoon": function (persoon) {
+        return JSON.parse(localStorage.getItem(persoon))
+    },
+    "getGeselecteerd_persoon": function () {
+        return JSON.parse(localStorage.getItem("letterkuil-geselekteerd_persoon"))
+    },
+    "getGeselecteerde_letters": function () {
+        return JSON.parse(localStorage.getItem("letterkuil-geselekteerde_letters"))
+    },
+    "setLijst_met_personen": function (data) {
+        localStorage.setItem("letterkuil-lijst_personen", JSON.stringify(data))
+    },
+    "setPersoon": function (persoon, data) {
+        return localStorage.setItem(persoon, JSON.stringify(data))
+    },
+    "setGeselecteerd_persoon": function (data) {
+        return localStorage.setItem("letterkuil-geselekteerd_persoon", JSON.stringify(data))
+    },
+    "setGeselecteerde_letters": function (data) {
+        return localStorage.setItem("letterkuil-geselekteerde_letters", JSON.stringify(data))
+    },
+    "check": function () {
+        if (!localStorage.getItem("letterkuil-lijst_personen")) {
+            localStorage.setItem("letterkuil-lijst_personen", JSON.stringify([]))
+        }
+    },
+    "remove": function (key) {
+        localStorage.removeItem(key)
     }
-}
-
-export function remove(key) {
-    localStorage.removeItem(key)
 }
